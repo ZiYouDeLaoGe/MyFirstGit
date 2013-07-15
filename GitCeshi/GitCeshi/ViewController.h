d524164b152811b0d6194d44530dd699c2a427bf
LiRenbing <lirenbing@inspiry.cn>
2013-07-15 17:33:58 +0800

    初始版本
    git用法测试

---
+++ GitCeshi/GitCeshi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitCeshi
+//
+//  Created by demo on 13-7-15.
+//  Copyright (c) 2013年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitCeshi/GitCeshi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitCeshi
+//
+//  Created by demo on 13-7-15.
+//  Copyright (c) 2013年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
